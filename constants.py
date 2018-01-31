74b769198ad7b7a29f7a1a4fd1c94ac64a378a07
Tommy Oldfield <koaxuaudio@gmail.com>
2018-01-31 11:34:00 +0000

    Updated comments
    Updated comments to help user in more detail

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-iDrive = "" # Drive letter of iPod including colon (:)
+iDrive = "" # Drive letter of iPod including colon (:), e.g. ("C:"
 pcDest = "{}\\iPod_Control\\iTunes\\Play Counts".format(iDrive) # Play Count path
-copyDest = "" # Destination to copy Play Count file
-scrobDest = "" # Path to where QTScrobbler is installed
-iPath = "" # Path to where iTunes is installed
+copyDest = "" # Destination to copy Play Count file, including drive letter and double slashes \\, e.g. "C:\\Users\\user\\Desktop"
+scrobDest = "" # Path to where QTScrobbler is installed, including drive letter and double slashes \\, e.g. C:\\qtscrobbler.exe"
+iPath = "" # Path to where iTunes is installed, including drive letter and double slashes \\, e.g. C:\\iTunes\\itunes.exe"
